54e3d9d14a2f62d0008105066771f46cd11ae916
wangzhixin <869265377@qq.com>
2022-01-14 11:33:17 +0800

    100%

--- webApi/app/index/config/config.php
+++ webApi/app/index/config/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'title' => '医疗机构新型冠状病毒肺炎筛查表 COVID-19 Screening Questionnaire for Healthcare Facilities',
-    'appid'=>'',
+    'appid'=>'wxcb042cc88e8c6dc4',
     'appsecret'=>'',
-    'web_url'=>'http://scancode.com',
+    'web_url'=>'http://scancode.pureplusclinic.com',
 ];
